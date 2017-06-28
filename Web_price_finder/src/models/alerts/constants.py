ada8a29689cd1a1dd0a66be3762073409c281280
FullM3talSmurf <FullM3talSmurf@gmail.com>
2017-06-28 14:34:35 +1000

    Add files via upload

---
+++ Web_price_finder/src/models/alerts/constants.py
@@ -0,0 +1,8 @@
+
+
+URL = "https://api.mailgun.net/v3/sandboxe00af8670282400d827290d19b5fdb60.mailgun.org/messages"
+API_KEY = "key-23be662af53b83185822cef297460c00"
+FROM = "Mailgun Sandbox <postmaster@sandboxe00af8670282400d827290d19b5fdb60.mailgun.org>"
+
+ALERT_TIMEOUT = 10
+COLLECTION = "alerts"
